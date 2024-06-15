e2a8c0d2c39639fb826a7b70807b57714efa43a0
RexEdThan <120693737+RexEdThan@users.noreply.github.com>
2024-06-15 10:00:55 +0800

    Update load_steps.py

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -1,133 +1,58 @@
-Feature: The product store service back-end
-    As a Product Store Owner
-    I need a RESTful catalog service
-    So that I can keep track of all my products
+######################################################################
+# Copyright 2016, 2023 John J. Rofrano. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+######################################################################
 
-Background:
-    Given the following products
-        | name       | description     | price   | available | category   |
-        | Hat        | A red fedora    | 59.95   | True      | CLOTHS     |
-        | Shoes      | Blue shoes      | 120.50  | False     | CLOTHS     |
-        | Big Mac    | 1/4 lb burger   | 5.99    | True      | FOOD       |
-        | Sheets     | Full bed sheets | 87.00   | True      | HOUSEWARES |
+"""
+Product Steps
 
-Scenario: The server is running
-    When I visit the "Home Page"
-    Then I should see "Product Catalog Administration" in the title
-    And I should not see "404 Not Found"
+Steps file for products.feature
 
-Scenario: Create a Product
-    When I visit the "Home Page"
-    And I set the "Name" to "Hammer"
-    And I set the "Description" to "Claw hammer"
-    And I select "True" in the "Available" dropdown
-    And I select "Tools" in the "Category" dropdown
-    And I set the "Price" to "34.95"
-    And I press the "Create" button
-    Then I should see the message "Success"
-    When I copy the "Id" field
-    And I press the "Clear" button
-    Then the "Id" field should be empty
-    And the "Name" field should be empty
-    And the "Description" field should be empty
-    When I paste the "Id" field
-    And I press the "Retrieve" button
-    Then I should see the message "Success"
-    And I should see "Hammer" in the "Name" field
-    And I should see "Claw hammer" in the "Description" field
-    And I should see "True" in the "Available" dropdown
-    And I should see "Tools" in the "Category" dropdown
-    And I should see "34.95" in the "Price" field
+For information on Waiting until elements are present in the HTML see:
+    https://selenium-python.readthedocs.io/waits.html
+"""
+import requests
+from behave import given
 
-Scenario: Read a Product
-    When I visit the "Home Page"
-    And I set the "Name" to "Hat"
-    And I press the "Search" button
-    Then I should see the message "Success"
-    When I copy the "Id" field
-    And I press the "Clear" button
-    And I paste the "Id" field
-    And I press the "Retrieve" button
-    Then I should see the message "Success"
-    And I should see "Hat" in the "Name" field
-    And I should see "A red fedora" in the "Description" field
-    And I should see "True" in the "Available" dropdown
-    And I should see "Cloths" in the "Category" dropdown
-    And I should see "59.95" in the "Price" field
+# HTTP Return Codes
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_204_NO_CONTENT = 204
 
-Scenario: Update a Product
-    When I visit the "Home Page"
-    And I set the "Name" to "Hat"
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "A red fedora" in the "Description" field
-    When I change "Name" to "Fedora"
-    And I press the "Update" button
-    Then I should see the message "Success"
-    When I copy the "Id" field
-    And I press the "Clear" button
-    And I paste the "Id" field
-    And I press the "Retrieve" button
-    Then I should see the message "Success"
-    And I should see "Fedora" in the "Name" field
-    When I press the "Clear" button
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "Fedora" in the results
-    And I should not see "Hat" in the results
+@given('the following products')
+def step_impl(context):
+    """ Delete all Products and load new ones """
+    #
+    # List all of the products and delete them one by one
+    #
+    rest_endpoint = f"{context.base_url}/products"
+    context.resp = requests.get(rest_endpoint)
+    assert(context.resp.status_code == HTTP_200_OK)
+    for product in context.resp.json():
+        context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
+        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
 
-Scenario: Delete a Product
-    When I visit the "Home Page"
-    And I set the "Name" to "Hat"
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "A red fedora" in the "Description" field
-    When I copy the "Id" field
-    And I press the "Clear" button
-    And I paste the "Id" field
-    And I press the "Delete" button
-    Then I should see the message "Product has been Deleted!"
-    When I press the "Clear" button
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should not see "Hat" in the results
-
-Scenario: List all products
-    When I visit the "Home Page"
-    And I press the "Clear" button
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "Hat" in the results
-    And I should see "Shoes" in the results
-    And I should see "Big Mac" in the results
-    And I should see "Sheets" in the results
-
-Scenario: Search by category
-    When I visit the "Home Page"
-    And I press the "Clear" button
-    And I select "Food" in the "Category" dropdown
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "Big Mac" in the results
-    And I should not see "Hat" in the results
-    And I should not see "Shoes" in the results
-    And I should not see "Sheets" in the results
-
-Scenario: Search by available
-    When I visit the "Home Page"
-    And I press the "Clear" button
-    And I select "True" in the "Available" dropdown
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "Hat" in the results
-    And I should see "Big Mac" in the results
-    And I should see "Sheets" in the results
-    And I should not see "Shoes" in the results
-
-Scenario: Search by name
-    When I visit the "Home Page"
-    And I set the "Name" to "Hat"
-    And I press the "Search" button
-    Then I should see the message "Success"
-    And I should see "Hat" in the "Name" field
-    And I should see "A red fedora" in the "Description" field
+    #
+    # load the database with new products
+    #
+    for row in context.table:
+        payload = {
+            "name": row['name'],
+            "description": row['description'],
+            "price": row['price'],
+            "available": row['available'] in ['True', 'true', '1'],
+            "category": row['category']
+        }
+        context.resp = requests.post(rest_endpoint, json=payload)
+        assert context.resp.status_code == HTTP_201_CREATED
